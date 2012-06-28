9991d69a72b743051237bd11a44539a7423a0b1b
Daniel Quimper <dquimper@gmail.com>
2012-06-27 23:13:06 -0400

    Initial Commit

---
+++ Redis/Redis.h
@@ -0,0 +1,15 @@
+//
+//  Redis.h
+//  Redis
+//
+//  Created by Daniel Quimper on 12-06-27.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface Redis : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ Redis/Redis.m
@@ -0,0 +1,17 @@
+//
+//  Redis.m
+//  Redis
+//
+//  Created by Daniel Quimper on 12-06-27.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Redis.h"
+
+@implementation Redis
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
